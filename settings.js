484018cd8f40816321c4d52502ff8337ae93336f
Hopfenbraeu <u.schneider@hofbraeu-wirtshaus.de>
2019-01-14 15:11:59 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ exports.senderName = "Hofbräu Wirtshaus Newsletter";
 
 // set 'exports.listId = null' to add contact to all contacts, but no specific list
 // or a string with the listId to add to a specific list
-exports.listId = null;
+exports.listId = 6513599;
 
 // set 'exports.templateId = null' to opt out of using a template
 // or a string with the templateId to use a template
